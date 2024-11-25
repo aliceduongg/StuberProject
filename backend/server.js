762c664c82f8e008e95749b07426dc207b66eac6
liceyoung <aliceduong841@gmail.com>
2024-11-25 15:39:59 -0600

    add Middleware, database connection in server side

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,32 @@
 // creating backend API
 const express = require('express');
 const app = express();
-const PORT = 8080;
 const cors = require('cors');
+const dotenv = require('dotenv');
 
+// to access .env file
+dotenv.config(); 
+
+// Middleware 
 app.use(cors());
+app.use`express.json()`; //  to parse json data 
+
+// Connect to mongoose database
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParse: true,
+    useUnifiedTopology: true,
+})
+.then (() => console.log('Connected to MongoDB'))
+.catch (() => console.error ('Could not connect to MongoDB'));
+
+
 
 app.get("/api/home", (req, res) => {
     res.json({ message: 'Hello from server!' });
 }
 );
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
